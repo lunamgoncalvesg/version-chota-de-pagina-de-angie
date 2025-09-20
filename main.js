a0059289bbff1e0d6dd8abb6cbeb3cfe0fa975eb
lunamgoncalvesg <lunamgoncalvesguerreiro@gmail.com>
2025-09-20 11:58:10 -0300

    version vieja

---
+++ main.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('nav ul li a')
+    .forEach(link => link.addEventListener('click', () => change(link.dataset.id, link)))
+
+function change(id, link) {
+    let panels = document.querySelectorAll('main > div')
+    panels.forEach(p => p.classList.remove('active'))
+    document.getElementById(id).classList.add('active')
+
+    document.querySelectorAll('nav ul li a').forEach(a => a.classList.remove('active'))
+    link.classList.add('active')
+}
+
+let cart = document.querySelector('#carrito');
+let inicio = document.querySelector('#inicio');
+let btns = document.querySelectorAll('#comprar, #volver');
+btns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        cart.classList.toggle('active');
+        inicio.classList.toggle('active');
+    });
+});
