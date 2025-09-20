cf99b7494f0085f2e4a4a3c2085be41200b78356
lunamgoncalvesg <lunamgoncalvesguerreiro@gmail.com>
2025-09-20 12:28:32 -0300

    sigue siendo feo

--- main.js
+++ main.js
@@ -5,17 +5,16 @@ function change(id, link) {
     let panels = document.querySelectorAll('main > div')
     panels.forEach(p => p.classList.remove('active'))
     document.getElementById(id).classList.add('active')
-
     document.querySelectorAll('nav ul li a').forEach(a => a.classList.remove('active'))
     link.classList.add('active')
 }
 
-let cart = document.querySelector('#carrito');
-let inicio = document.querySelector('#inicio');
-let btns = document.querySelectorAll('#comprar, #volver');
+let cart = document.querySelector('#cart');
+let home = document.querySelector('#home');
+let btns = document.querySelectorAll('#buy, #back');
 btns.forEach(btn => {
     btn.addEventListener('click', e => {
         cart.classList.toggle('active');
-        inicio.classList.toggle('active');
+        home.classList.toggle('active');
     });
 });
